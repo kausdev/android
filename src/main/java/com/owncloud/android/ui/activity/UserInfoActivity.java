2b46382d05bea148c7d036a34442b47a9cd0eca5
Mario Danic <mario@lovelyhq.com>
2017-07-23 21:19:51 +0200

    More work
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
@@ -62,7 +62,6 @@ import com.owncloud.android.authentication.AuthenticatorActivity;
 import com.owncloud.android.datamodel.ArbitraryDataProvider;
 import com.owncloud.android.datamodel.PushConfigurationState;
 import com.owncloud.android.datamodel.SyncedFolderProvider;
-import com.owncloud.android.datamodel.UploadsStorageManager;
 import com.owncloud.android.lib.common.UserInfo;
 import com.owncloud.android.lib.common.operations.RemoteOperation;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
@@ -405,11 +404,6 @@ public class UserInfoActivity extends FileActivity {
                                     SyncedFolderProvider syncedFolderProvider = new SyncedFolderProvider(
                                             contentResolver);
                                     syncedFolderProvider.deleteSyncFoldersForAccount(account);
-
-                                    UploadsStorageManager uploadsStorageManager = new UploadsStorageManager(
-                                            contentResolver, getActivity());
-                                    uploadsStorageManager.cancelPendingAutoUploadJobsForAccount(account);
-                                    uploadsStorageManager.removeAccountUploads(account);
 
                                     // disable daily backup
                                     ArbitraryDataProvider arbitraryDataProvider = new ArbitraryDataProvider(
